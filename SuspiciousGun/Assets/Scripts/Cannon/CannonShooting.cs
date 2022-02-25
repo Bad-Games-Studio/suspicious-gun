11947d949dabb2344d9953fbccc26a8fd175efc5
DOCtorActoAntohich <fairy666death@mail.ru>
2022-02-25 18:04:43 +0300

    Add an epic cannon from Asset Store

---
+++ SuspiciousGun/Assets/Scripts/Cannon/CannonShooting.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Cannon
+{
+    public class CannonShooting : MonoBehaviour
+    {
+        public Vector2 horizontalAngleRange;
+        public Vector2 verticalAngleRange;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
